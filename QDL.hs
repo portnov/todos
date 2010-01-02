925db7b32934034540ddc55c7908f8b251f62032
Ilya Portnov <portnov84@rambler.ru>
2010-01-02 11:31:44 +0500

    delete old sources..

--- QDL.hs
+++
@@ -1,9 +0,0 @@
-module QDL where
-
-import Data.Tree
-import TodoParser
-
-select pred todos = filter pred $ map (select' pred) todos
-
-select' pred todo@(Node item children) | pred todo = Node item (select pred children)
-                                       | otherwise = Node item []
