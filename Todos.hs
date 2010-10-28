8649ba89ccc67b714486287838133c03541c42c6
Ilya V. Portnov <i.portnov@compassplus.ru>
2010-10-28 14:24:22 +0600

    Simplify API visible from Todos module.

--- Todos.hs
+++ Todos.hs
@@ -1,25 +1,26 @@
 module Todos 
   (module Todos.Unicode,
    module Todos.Types,
-   module Todos.Loader,
-   module Todos.Tree,
-   module Todos.CommandParser,
    module Todos.Config,
-   module Todos.ConfigUtils,
-   module Todos.CmdLine,
-   module Todos.Dot,
-   module Todos.Main,
+   emptyBaseConfig,
+   emptyConfig,
+   defaultConfig,
+   defaultTodosFilter,
+   parseCmdLine,
+   getColor,
+   getShape,
+   defaultPrintTodos,
+   todos,
    getCurrentDateTime) where
 
 import Todos.Unicode
 import Todos.Types
-import Todos.Loader
-import Todos.Tree
-import Todos.CommandParser
 import Todos.Config
 import Todos.ConfigUtils
-import Todos.CmdLine
 import Todos.Dates (getCurrentDateTime)
-import Todos.Dot
+import Todos.Color
+import Todos.Shapes
+import Todos.Print
 import Todos.Main
+
 
