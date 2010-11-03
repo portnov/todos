cda9628d6dec4305a466207c0127fdb43986e250
Ilya V. Portnov <i.portnov@compassplus.ru>
2010-11-03 12:38:09 +0500

    Support more DOT node shapes.

--- Todos/Shapes.hs
+++ Todos/Shapes.hs
@@ -8,22 +8,66 @@ import Todos.Types
 -- | Supported node shapes for DOT output
 data Shape = 
     Box 
+  | Box3D
+  | Component
+  | Square
+  | MSquare
   | Ellipse
   | Diamond
+  | MDiamond
+  | Circle
   | DCircle
+  | MCircle
   | Note
   | Parallelogram
+  | Tab
   | Folder
+  | Polygon Int
+  | Point
+  | Egg
+  | Triangle Bool    -- ^ Inverted?
+  | PlainText
+  | Trapezium Bool   -- ^ Inverted?
+  | House Bool       -- ^ Inverted?
+  | Pentagon
+  | Hexagon
+  | Septagon
+  | Octagon Int      -- ^ Simple, double, triple?
   deriving (Eq)
 
 instance Show Shape where
-  show Box           = "box"
-  show Ellipse       = "ellipse"
-  show Diamond       = "diamond"
   show DCircle       = "doublecircle"
-  show Note          = "note"
-  show Parallelogram = "parallelogram"
-  show Folder        = "folder"
+  show (Polygon k)   = "polygon\", sides=\"" ++ show k 
+  show (Triangle True) = "invtriangle"
+  show (Triangle False) = "triangle"
+  show (Trapezium True) = "invtrapezium"
+  show (Trapezium False) = "trapezium"
+  show (House True)  = "invhouse"
+  show (House False) = "house"
+  show (Octagon 1)   = "octagon"
+  show (Octagon 2)   = "doubleoctagon"
+  show (Octagon 3)   = "tripleoctagon"
+  show (Octagon _)   = error "Only 1,2 or 3 are supported as Octagon arguments!"
+  show Box  = "Box "
+  show Box3D = "Box3D"
+  show Component = "Component"
+  show Square = "Square"
+  show MSquare = "MSquare"
+  show Ellipse = "Ellipse"
+  show Diamond = "Diamond"
+  show Circle = "Circle"
+  show MDiamond = "MDiamond"
+  show MCircle = "MCircle"
+  show Note = "Note"
+  show Parallelogram = "Parallelogram"
+  show Tab = "Tab"
+  show Folder = "Folder"
+  show Point = "Point"
+  show Egg = "Egg"
+  show PlainText = "PlainText"
+  show Pentagon = "Pentagon"
+  show Hexagon = "Hexagon"
+  show Septagon = "Septagon"
 
 -- | Node shapes for some common item statuses
 shapes ∷ M.Map String Shape
